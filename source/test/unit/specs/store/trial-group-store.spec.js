0c90a7cc417a78ef3a722540c099181f6352c3a0
tri-chandra <tri-chandra@users.noreply.github.com>
2018-03-08 17:06:09 +1100

    Unit test started for trial-group-store

---
+++ source/test/unit/specs/store/trial-group-store.spec.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai'
+import store from '@/store/modules/trial-group-store'
+
+describe('mutations', () => {
+  it('addGroup', () => {
+    const state = {
+      groups: [],
+      idCounter: 0
+    }
+
+    store.mutations.addGroup(state, {a: 1})
+
+    expect(state.groups.length).to.equal(1)
+  })
+})
